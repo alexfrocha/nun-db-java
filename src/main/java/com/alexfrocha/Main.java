0aa5f8c5169776bee8ebf12ede013a2df9753f7a
alexfrocha <alex.raskav@gmail.com>
2024-07-17 19:40:11 -0300

    none: starting 'nun-db-java' project

---
+++ src/main/java/com/alexfrocha/Main.java
@@ -0,0 +1,10 @@
+package com.alexfrocha;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome! \n");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
