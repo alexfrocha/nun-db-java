6edf615aad566bd2f739ff1af871ce8b6e765715
alexfrocha <alex.raskav@gmail.com>
2024-07-20 02:57:50 -0300

    security: verifying if the user is connected with a db before any method

--- src/main/java/com/alexfrocha/Main.java
+++ src/main/java/com/alexfrocha/Main.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
         NunDB db = new NunDB("ws://localhost:3012", "alex", "alex");
-        db.showLogs(true);
-        db.useDb("aware", "aware");
+//        db.showLogs(true);
+//        db.useDb("aware", "aware");
         db.addWatch("dizoiprovideo", novoValor -> {
             System.out.println("o valor 'dizoiprovideo' agora virou: " + novoValor);});
         db.set("dizoiprovideo", "oi video 1");
@@ -17,7 +17,7 @@ public class Main {
 
 
         List<String> databases = db.getAllDatabases().join();
-        System.out.println(databases.size());
+        System.out.println(databases);
 
         while(true) {}
     }
