b40a884aef36fef951097425ffda0723c3f9b075
alexfrocha <alex.raskav@gmail.com>
2024-07-20 01:16:05 -0300

    info: interfaces.ResolveCallback, cause i dont see a use for it right now and therefore

--- src/main/java/com/alexfrocha/Main.java
+++ src/main/java/com/alexfrocha/Main.java
@@ -1,5 +1,7 @@
 package com.alexfrocha;
 
+import com.alexfrocha.enums.Permissions;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         NunDB db = new NunDB("ws://localhost:3012", "alex", "alex");
--- src/main/java/com/alexfrocha/interfaces/ResolveCallback.java
+++
@@ -1,5 +0,0 @@
-package com.alexfrocha.interfaces;
-
-public interface ResolveCallback {
-    void resolve(String command, String value);
-}
